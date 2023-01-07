8cc520f681b2816af3b002638d0346067c6f5d78
renjun0324 <312827559@qq.com>
2023-01-08 01:09:45 +0800

    add murp_kmeans_pca

--- R/murp.R
+++ R/murp.R
@@ -29,7 +29,13 @@ MURP <- function(Data,
                  fast = FALSE,
                  cluster_iter_max = 1000){
 
-  f = MURP.Kmeans(Data = Data, cores = cores, iter = iter, omega = omega, seed = seed, fast = fast, cluster_iter_max = cluster_iter_max)
+  f = MURP.Kmeans(Data = Data,
+                  cores = cores,
+                  iter = iter,
+                  omega = omega,
+                  seed = seed,
+                  fast = fast,
+                  cluster_iter_max = cluster_iter_max)
   return(f)
 }
 
@@ -71,7 +77,46 @@ murp_specific <- function(Data = NULL,
 }
 
 
+#' MURP
+#'
+#' @description
+#' This new method is optimized from the MURP algorithm, it's more efficient.
+#' Loglike: Prior probability estimated by likelyhood function;
+#' MURP: Bayesian Information Criterions value;
+#' Recommended_K: Recommended K value.
+#'
+#' @param Data expression matrix with Cells * Genes
+#' @param type data type, "expr" or "pca"
+#' @param omega omega value in pseudo-BIC calculating
+#' @param iter the number of iterations
+#' @param cores the number of cores to use in multi-threads calculating, default is 1.
+#' Multi-threaded calculation is not recommended except for users with server support
+#' @param seed random seed, default is 723
+#' @param fast speed up calculations
+#' @param cluster_iter_max kmeans iterations
+#'
+#' @return a list include recommend_K, object of clustering result and so on
+#'
+#' @export
+#'
 
+MURP <- function(Data,
+                 omega = 1/6,
+                 iter = 20,
+                 cores = 1,
+                 seed = 723,
+                 fast = FALSE,
+                 cluster_iter_max = 1000){
+
+  f = MURP.Kmeans(Data = Data,
+                  cores = cores,
+                  iter = iter,
+                  omega = omega,
+                  seed = seed,
+                  fast = fast,
+                  cluster_iter_max = cluster_iter_max)
+  return(f)
+}
 
 
 
--- R/murp_Kmeans.R
+++ R/murp_Kmeans.R
@@ -101,10 +101,14 @@ MURP.Kmeans <- function(Data,
 
   Recommended_K <- k[which(BIC == min(BIC))[1]]
 
-  command <- paste('Data =',dim(Data)[1],'*',dim(Data)[2],
-                   'omega =', omega,
-                   'cores =',cores,
-                   'iter =',iter)
+  command <- paste("Function: MURP.Kmeans",
+                   'Data =',dim(Data)[1],'*',dim(Data)[2],
+                   '| omega =', omega,
+                   '| cores =',cores,
+                   '| iter =',iter,
+                   '| fast = ', fast,
+                   '| seed = ', seed,
+                   '| cluster_iter_max = ', cluster_iter_max)
 
   final <- list(k = k,
                 Step = Step,
@@ -115,7 +119,155 @@ MURP.Kmeans <- function(Data,
                 Recommended_K_cl = clsSave[[Recommended_K]],
                 Command = command,
                 cl = clsSave,
-                rawdata = Data)
+                rawdata = Data,
+                date = date())
+
+  cat('BIC completed         ',date(),'\n')
+  return(final)
+}
+
+#' MURP.Kmeans.pca
+#' @importFrom parallel makeCluster stopCluster clusterExport parLapply
+#' @importFrom NPflow mvnpdfC irlba
+#'
+#' @export
+#' @noRd
+#'
+MURP.Kmeans.pca <- function(Data,
+                            cores = 1,
+                            iter = 20,
+                            omega = 1/50,
+                            seed = 723,
+                            fast = FALSE,
+                            cluster_iter_max = 1000,
+                            center = F,
+                            scale = F,
+                            pc_number = 30){
+
+  cat('Initiating ...        ',date(),'\n')
+  CellNum <- dim(Data)[1]
+  GeneNum <- dim(Data)[2]
+
+  cat('PCA ...        ',date(),'\n')
+  set.seed(seed)
+  pca = irlba::prcomp_irlba(Data, center = center, scale. = center, n = pc_number)
+  pca_dat = pca$x[,1:pc_number]
+
+  cat('K-Means calculating   ',date(),'\n')
+  clsSave <- list()
+  k <- c()
+  Loglike <- c()
+  BIC <- c()
+  Step <- c()
+  Penalty <- c()
+
+  maxK <- round(CellNum * 1)
+  K_series <- ceiling(quantile(1:maxK, 0.5))
+  min_k <- round(K_series * 1)
+  step <- 0
+
+  for (i in 1:iter) {
+    cat('Iter',i,'              ',date(),'\n')
+
+    if (i != 1 & step <= 1) {
+      cat('Step less than 1,stop iter \n')
+      break()
+    }
+
+    if (i != 1) {
+      min_k <- k[which(BIC == min(BIC))]
+    }
+
+    step <- (maxK*1) * 1/2^(i + 1)
+    K_series <- ceiling(c(max(min_k - step, 2),
+                          min(min_k + step,maxK - 1)))
+    cat('Step:',step,'  k:',min_k,'\n')
+    cat(K_series,'\n')
+
+    cl <- makeCluster(cores)
+    clusterExport(cl, c("seed", "Data", "pca_dat", "K_series"), envir = environment())
+    cls <- parLapply(cl, K_series, function(Centers){
+      set.seed(seed)
+      tmp = kmeans(pca_dat, centers = Centers, iter.max = cluster_iter_max)
+      cluster = tmp$cluster
+      cluster_factor = factor(cluster, levels = 1:length(unique(cluster)))
+      tapply(1:CellNum, cluster_factor,
+             function(x, y) {
+               # Data[x,,drop=F]
+               apply(Data[x,,drop=F], 2, mean)
+               }) -> tmplist
+      centers = do.call(rbind, tmplist)
+      return(list(cluster = cluster, centers = centers))
+    })
+    stopCluster(cl)
+
+    gc()
+
+    for(r in 1:length(cls) ){
+      clsSave[[dim(cls[[r]]$centers)[1]]] <- cls[[r]]
+    }
+
+    K_series_len <- length(K_series)
+    if(fast){
+      Loglike_iter <-  unlist(pblapply(1:K_series_len,
+                                       function(l){FastLogLikly(Data = Data,
+                                                                centers = cls[[l]]$centers,
+                                                                cluster = cls[[l]]$cluster,
+                                                                K = K_series[l]) }))
+    }else{
+      Loglike_iter <-  unlist(pblapply(1:K_series_len,
+                                       function(l){LogLikly(Data = Data,
+                                                            centers = cls[[l]]$centers,
+                                                            cluster = cls[[l]]$cluster,
+                                                            K = K_series[l],
+                                                            cores = cores ) }))
+    }
+
+    a <- 1 + 1e-9
+    gamma <- sqrt(a * K_series * K_series * log(GeneNum))
+    if(fast){
+      penalty <- (1/100 * omega) * (1 + gamma) * log(GeneNum) * K_series
+    }else{
+      penalty <- (6 * omega) * (1 + gamma) * log(GeneNum) * K_series
+    }
+
+    BIC_iter <- penalty - 2 * Loglike_iter
+    k <- append(k, K_series)
+    Penalty <- append(Penalty, penalty)
+    Loglike <- append(Loglike, Loglike_iter)
+    BIC <- append(BIC, BIC_iter)
+    Step <- append(Step, step)
+
+    if ((min_k - step < 1 || min_k + step > CellNum - 1)) {
+      break
+    }
+  }
+
+  Recommended_K <- k[which(BIC == min(BIC))[1]]
+
+  command <- paste("Function: MURP.Kmeans.pca",
+                   '| Data =',dim(Data)[1],'*',dim(Data)[2],
+                   '| omega =', omega,
+                   '| cores =',cores,
+                   '| iter =',iter,
+                   '| fast = ', fast,
+                   '| seed = ', seed,
+                   '| cluster_iter_max = ', cluster_iter_max,
+                   '| center =', center,
+                   '| scale =', scale,
+                   '| pc_number =', pc_number)
+
+  final <- list(k = k,
+                Step = Step,
+                Loglike = Loglike,
+                BIC = BIC,
+                Penalty = Penalty,
+                Recommended_K = Recommended_K,
+                Recommended_K_cl = clsSave[[Recommended_K]],
+                Command = command,
+                cl = clsSave,
+                rawdata = Data,
+                date = date())
 
   cat('BIC completed         ',date(),'\n')
   return(final)
@@ -168,10 +320,13 @@ murp_specific_Kmeans <- function(Data,
   penalty <- (6 * omega) * (1 + gamma) * log(GeneNum) * K
   BIC_iter <- penalty - 2 * Loglike_iter
 
-  command <- paste('Data =', dim(Data)[1],'*',dim(Data)[2],
-                   'omega = ', omega,
-                   'cores =', cores,
-                   'K =', K)
+  command <- paste("Function: murp_specific_Kmeans",
+                   '| Data =', dim(Data)[1],'*',dim(Data)[2],
+                   '| omega = ', omega,
+                   '| cores =', cores,
+                   '| K =', K,
+                   '| fast =', fast,
+                   '| cluster_iter_max = ',cluster_iter_max)
 
   final <- list(k = K,
                 Loglike = Loglike_iter,
@@ -179,7 +334,8 @@ murp_specific_Kmeans <- function(Data,
                 Penalty = penalty,
                 Recommended_K = K,
                 Command = command,
-                cl = cls)
+                cl = cls,
+                date = date())
 
   cat('BIC completed         ',date(),'\n')
 
